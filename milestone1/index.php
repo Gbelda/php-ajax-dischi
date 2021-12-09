a5e4f9ac10c7cd28a597075625aa6c10edc708e0
gbelda <gbelda56@gmail.com>
2021-12-09 14:36:21 +0100

    Complete base layout and structure
    Need refactoring

--- milestone1/index.php
+++ milestone1/index.php
@@ -17,60 +17,110 @@ $albums= [
     "genre"=> "Pop",
     "year"=> "1992"
 ],
+
 [
-"poster"=> "https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg",
-"title"=> "Ten's Summoner's Tales",
-"author"=> "Sting",
-"genre"=> "Pop",
-"year"=> "1993"
+    "poster"=> "https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg",
+    "title"=> "Ten's Summoner's Tales",
+    "author"=> "Sting",
+    "genre"=> "Pop",
+    "year"=> "1993"
 ],
+
 [
-"poster"=> "https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg",
-"title"=> "Steve Gadd Band",
-"author"=> "Steve Gadd Band",
-"genre"=> "Jazz",
-"year"=> "2018"
+    "poster"=> "https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg",
+    "title"=> "Steve Gadd Band",
+    "author"=> "Steve Gadd Band",
+    "genre"=> "Jazz",
+    "year"=> "2018"
 ],
+
 [
-"poster"=> "https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg",
-"title"=> "Brave new World",
-"author"=> "Iron Maiden",
-"genre"=> "Metal",
-"year"=> "2000"
+    "poster"=> "https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg",
+    "title"=> "Brave new World",
+    "author"=> "Iron Maiden",
+    "genre"=> "Metal",
+    "year"=> "2000"
 ],
+
 [
-"poster"=> "https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg",
-"title"=> "One more car, one more raider",
-"author"=> "Eric Clapton",
-"genre"=> "Rock",
-"year"=> "2002"
+    "poster"=> "https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg",
+    "title"=> "One more car, one more raider",
+    "author"=> "Eric Clapton",
+    "genre"=> "Rock",
+    "year"=> "2002"
 ],
+
 [
-"poster"=> "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
-"title"=> "Made in Japan",
-"author"=> "Deep Purple",
-"genre"=> "Rock",
-"year"=> "1972"
+    "poster"=> "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
+    "title"=> "Made in Japan",
+    "author"=> "Deep Purple",
+    "genre"=> "Rock",
+    "year"=> "1972"
 ],
+
 [
-"poster"=> "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
-"title"=> "And Justice for All",
-"author"=> "Metallica",
-"genre"=> "Metal",
-"year"=> "1988"
+    "poster"=> "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
+    "title"=> "And Justice for All",
+    "author"=> "Metallica",
+    "genre"=> "Metal",
+    "year"=> "1988"
 ],
+
 [
-"poster"=> "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
-"title"=> "Hard Wired",
-"author"=> "Dave Weckl",
-"genre"=> "Jazz",
-"year"=> "1994"
+    "poster"=> "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
+    "title"=> "Hard Wired",
+    "author"=> "Dave Weckl",
+    "genre"=> "Jazz",
+    "year"=> "1994"
 ],
+
 [
-"poster"=> "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
-"title"=> "Bad",
-"author"=> "Michael Jacjson",
-"genre"=> "Pop",
-"year"=> "1987"
+    "poster"=> "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
+    "title"=> "Bad",
+    "author"=> "Michael Jacjson",
+    "genre"=> "Pop",
+    "year"=> "1987"
 ],
+
 ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP DISCHI</title>
+    <link rel='stylesheet' type='text/css' href='./style.php' />
+</head>
+<body>
+
+    <header>
+        <div class="header container">
+            <img src="../milestone1/assets/img/spotify.png" alt="" />
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row d-flex flex-wrap justify-center">
+                <?php foreach ($albums as $album ) { ?>
+                    <div class="card">
+                        <div class="img_container">
+                            <img src="<?php echo $album['poster']?>" alt="">
+                        </div>
+                    <h3 class="title"><?php echo strtoupper($album['title']) ?></h3>
+                    <h5 class="meta_data">
+                        <?php echo $album['author']?>
+                        <span class="year"><?php echo $album['year']?></span>
+                    </h5>
+                    </div>
+               <?php } ?>
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
